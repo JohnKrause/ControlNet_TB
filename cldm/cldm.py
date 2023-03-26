5a31942ea5673635b238647a901113864be1f48b
Jack Krause <jack.krause@gmail.com>
2023-03-26 11:40:57 -0400

    opt

--- cldm/cldm.py
+++ cldm/cldm.py
@@ -421,8 +421,7 @@ class ControlLDM(LatentDiffusion):
             params += list(self.model.diffusion_model.out.parameters())
         opt = torch.optim.AdamW(params, lr=lr)
         sched=torch.optim.lr_scheduler.StepLR(opt,1,gamma=0.5)
-        #return  {'optimizer': opt, 'lr_scheduler': sched}
-        return opt
+        return  {'optimizer': opt, 'lr_scheduler': sched}
 
     def low_vram_shift(self, is_diffusing):
         if is_diffusing:
