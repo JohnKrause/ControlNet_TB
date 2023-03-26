6a09dc8217a3c1fbe9d4d03855ab58af0d80ffd3
Jack Krause <jack.krause@gmail.com>
2023-03-26 18:20:04 -0400

    test verified

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def download_from_s3():
     download_bucket_file(IMAGES_TARGET, IMAGES_TARGET_LOCAL, bucket)
     download_bucket_file(CONTROLS_TARGET, CONTROLS_TARGET_LOCAL, bucket)
 
-    #download manifest of images..
+    #download manifest of images...
 
     #extract things using multiprocessing
     print(f"Extracting {IMAGES_TARGET}...")
