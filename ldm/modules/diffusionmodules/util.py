ac41985c8af9ceae149c69509cdb41737cde4b5d
Jack Krause <jack.krause@gmail.com>
2023-03-26 14:11:19 -0400

    more

--- ldm/modules/diffusionmodules/util.py
+++ ldm/modules/diffusionmodules/util.py
@@ -170,7 +170,7 @@ def timestep_embedding(timesteps, dim, max_period=10000, repeat_only=False):
         print(f"b:{b}")
         c=a*b/half
         print(f"c:{c}")
-        freqs = torch.empty_like(c)
+        freqs = torch.empty_like(c, device=timesteps.device)
         for i in range(c.size(0)):
             freqs[i] = torch.exp(c[i])
             print(f"{c[i] , freqs[i]}")
