01419775f5e59da652199931e9e8b220837be949
Jack Krause <jack.krause@gmail.com>
2023-04-23 15:49:18 -0400

    more fixes

--- train.py
+++ train.py
@@ -11,42 +11,45 @@ from cldm.model import create_model, load_state_dict
 
 from config import *
 
+def start_train:
+	# Configs
+	revision = REVNUM
+	model_config = MODEL_CONFIG_LOCAL
+	start_model = MODEL_LOCAL
+	batch_size = 6
+	logger_freq = 3000
+	learning_rate = 1e-5
+	prompt_chance = 1.0
+	control_chance = 0.85
+	epoch_size=40000
+	max_epochs=20
+	sd_locked = True
+	only_mid_control = False
 
-# Configs
-revision = REVNUM
-model_config = MODEL_CONFIG_LOCAL
-start_model = MODEL_LOCAL
-batch_size = 6
-logger_freq = 3000
-learning_rate = 1e-5
-prompt_chance = 1.0
-control_chance = 0.85
-epoch_size=40000
-max_epochs=20
-sd_locked = True
-only_mid_control = False
+	set_sharing_strategy('file_system')
+	set_float32_matmul_precision('medium')
 
-set_sharing_strategy('file_system')
-set_float32_matmul_precision('medium')
-
-# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
-model = create_model(model_config).cpu()
-model.load_state_dict(load_state_dict(start_model, location='cpu'))
-model.learning_rate = learning_rate
-model.sd_locked = sd_locked
-model.only_mid_control = only_mid_control
+	# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
+	model = create_model(model_config).cpu()
+	model.load_state_dict(load_state_dict(start_model, location='cpu'))
+	model.learning_rate = learning_rate
+	model.sd_locked = sd_locked
+	model.only_mid_control = only_mid_control
 
 
-# Misc
-dataset = TB_Remote_Redis(REDIS_LIST,
-					epoch_size,
-					revision=revision,
-					prompt_chance=prompt_chance,
-					control_chance=control_chance)
-sampler = TB_Sampler(dataset, epoch_size)
-dataloader = DataLoader(dataset, num_workers=3, batch_size=batch_size, sampler=sampler)
-logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, callbacks=[logger], accumulate_grad_batches={0:100, 4:20, 11:5, 16:1}, max_epochs=max_epochs)
+	# Misc
+	dataset = TB_Remote_Redis(REDIS_LIST,
+						epoch_size,
+						revision=revision,
+						prompt_chance=prompt_chance,
+						control_chance=control_chance)
+	sampler = TB_Sampler(dataset, epoch_size)
+	dataloader = DataLoader(dataset, num_workers=3, batch_size=batch_size, sampler=sampler)
+	logger = ImageLogger(batch_frequency=logger_freq)
+	trainer = pl.Trainer(gpus=2, callbacks=[logger], accumulate_grad_batches={0:100, 4:20, 11:5, 16:1}, max_epochs=max_epochs)
 
-# Train!
-trainer.fit(model, dataloader)
+	# Train!
+	trainer.fit(model, dataloader)
+
+if __name__ == '__main__':
+	start_train()
